438cb9929b5fd7a6c0d53887c6f79873cc44cceb
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2019-09-17 21:39:34 -0300

    Remove setup

--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "Cripto Extreme Edition",
-    version = "2.0",
-    description = "Cripto Extreme Edition v2.0 by Cripto S.a",
-    executables = [Executable("run.py", base = None)])
